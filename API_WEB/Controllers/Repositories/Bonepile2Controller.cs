73cffb1fc5d539f618806a9298e54f525fa6fe49
unknown <V3201425@VNGZ.FOXCONN.COM>
2025-08-25 10:56:38 +0700

    up

--- API_WEB/Controllers/Repositories/Bonepile2Controller.cs
+++ API_WEB/Controllers/Repositories/Bonepile2Controller.cs
@@ -1312,7 +1312,7 @@ namespace API_WEB.Controllers.Repositories
                     A.WIP_GROUP LIKE '%B36R%'
                     AND B.MODEL_SERIAL = 'ADAPTER'
                     AND R107.WIP_GROUP NOT LIKE '%BR2C%'
-                    AND R107.WIP_GROUP NOT LIKE '%BCFA%'";
+                    AND R107.WIP_GROUP NOT LIKE '%BR2C%'";
 
             using var command = new OracleCommand(query, connection);
             using var reader = await command.ExecuteReaderAsync();
