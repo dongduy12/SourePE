a64d92f0552db423f1f0ca79295e2936e41f356c
dongduy12 <dongduy261299@gmail.com>
2025-08-28 08:41:20 +0700

    update scanlogs

---
+++ API_WEB/Controllers/App/ScanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_WEB.Controllers.App
+{
+    public class ScanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ API_WEB/ModelsDB/ScanLog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace API_WEB.ModelsDB
+{
+    public class ScanLog
+    {
+        public int Id { get; set; }
+        public string SerialNumber { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
